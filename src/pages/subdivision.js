4b72e3a63cef19cc7aa015eb2971c6c9a01f43d1
khang2905 <tvkhang2905@gmail.com>
2024-02-06 23:02:44 +0700

    Revert "Revert "CRUD Subdivision""
    This reverts commit cfec870ff725b78251dea12a9873e1383d5c5612.

--- src/pages/subdivision.js
+++ src/pages/subdivision.js
@@ -1,7 +1,12 @@
 import React from 'react';
-
+import Table from "../components/Table/SubdivisionTable"
+import SubdivisionTable from '../components/Table/SubdivisionTable';
 function SubdivisionPage() {
-  return <div>This is the Subdivision page</div>;
+  return (
+    <div>This is the Subdivision page</div>,
+    <SubdivisionTable/>
+  );
+    
 }
 
 export default SubdivisionPage;
